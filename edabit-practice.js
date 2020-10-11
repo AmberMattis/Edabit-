0c6f2352a78a4f48a5d2b62dc73b60892de8c15e
Amber Mattis <amber.mattis2@gmail.com>
2020-10-11 13:03:39 -0500

    edabit practice

--- edabit-practice.js
+++ edabit-practice.js
@@ -42,3 +42,29 @@
             return false;
     }
 }
+
+//In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+//
+// chickens = 2 legs
+// cows = 4 legs
+// pigs = 4 legs
+// The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals.
+
+    function animals(chickens, cows, pigs) {
+        var chickenLegs = chickens * 2;
+        var cowLegs = cows * 4;
+        var pigLegs = pigs * 4;
+        var totalLegs = chickenLegs + cowLegs + pigLegs;
+
+            return totalLegs;
+
+}
+
+//Write a function that returns the boolean true if the given number is zero, the string "positive" if the number is greater than zero or the string "negative" if it's smaller than zero.
+
+    function equilibrium (x) {
+        if (x > 0) return "positive";
+        if (x < 0)	return "negative";
+
+            return true;
+}
