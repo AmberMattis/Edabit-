3bc7e41d4d5bc9fd3bdc9561c09adbec05f58ba3
Amber Mattis <amber.mattis2@gmail.com>
2020-10-11 13:37:29 -0500

    edabit practice

--- edabit-practice.js
+++ edabit-practice.js
@@ -81,4 +81,10 @@
 
         function dividesEvenly(a, b) {
               return a % b  === 0;
-}
+    }
+
+//Create a function that takes an integer and return true if it's divisible by 100, otherwise return false.
+
+         function divisible(num) {
+             return num % 100 === 0;
+    }
