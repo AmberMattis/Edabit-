7c4dee6d476c1515ff5a2241a2ce2c1749be7c3b
Amber Mattis <amber.mattis2@gmail.com>
2020-10-11 14:30:29 -0500

    edabit practice

--- edabit-practice.js
+++ edabit-practice.js
@@ -117,3 +117,8 @@
         function isSameNum(num1, num2) {
              return num1 === num2 ? true : false;
     }
+
+// Given two arguments, return an array which contains these two arguments.
+        function makePair(num1, num2){
+            return [num1, num2];
+    }
