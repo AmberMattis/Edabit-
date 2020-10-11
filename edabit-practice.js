2d32b01147975e2e21e8cb184736e98d923636b6
Amber Mattis <amber.mattis2@gmail.com>
2020-10-10 20:58:04 -0500

    edabit practice

--- edabit-practice.js
+++ edabit-practice.js
@@ -2,6 +2,16 @@
 
 //return the remainder of two numbers
 
-function remainder(x, y) {
-    return x % y;
-}
+    function remainder(x, y) {
+         return x % y;
+    }
+
+//return a function that takes two integers (hours,minutes), converts them into seconds, and adds them.
+
+    function convert(hours, minutes) {
+        var hoursToSecs = hours * 3600;
+        var minutesToSecs = minutes * 60;
+        var totalSecs = hoursToSecs + minutesToSecs;
+
+        return totalSecs;
+    }
