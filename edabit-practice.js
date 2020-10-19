50485b5e0b1036c49cd238c3f9fd4faeafdbefac
Amber Mattis <amber.mattis2@gmail.com>
2020-10-19 05:48:20 -0500

    edabit practice

--- edabit-practice.js
+++ edabit-practice.js
@@ -250,4 +250,8 @@
     console.log(a);
     console.log(b);
 
-//
+//A bartender is writing a simple program to determine whether he should serve drinks to someone. He only serves drinks to people 18 and older and when he's not on break.
+    function shouldServeDrinks(age, onBreak) {
+        var serve = age >= 18 && !onBreak;
+        return serve;
+}
