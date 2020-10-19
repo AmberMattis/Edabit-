8472dd139f02e32f65d83002a4737e2ba81c7cb1
Amber Mattis <amber.mattis2@gmail.com>
2020-10-19 06:11:41 -0500

    edabit practice

--- edabit-practice.js
+++ edabit-practice.js
@@ -270,3 +270,11 @@
     function oddOrEven(s) {
         return s.length % 2 === 0
     }
+
+// Due to a programming concept known as truthiness, certain values can be evaluated to (i.e. take the place of) booleans. For example, 1 (or any number other than 0) is often equivalent to true, and 0 is often equivalent to false.
+//
+// Create a function that returns the opposite of the given boolean, as a number.
+
+    function flipBool(b) {
+        return b == true ? 0 : 1
+    }
