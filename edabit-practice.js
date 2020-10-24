eb80b8dfd5e94d82b66dd3def683b13b25d00223
Amber Mattis <amber.mattis2@gmail.com>
2020-10-24 10:48:05 -0500

    edabit practice

--- edabit-practice.js
+++ edabit-practice.js
@@ -297,3 +297,8 @@
     function posCom(num) {
         return Math.pow(2,num );
     }
+
+//Create a function that will put the first argument, a character, between every word in the second argument, a string
+    function add(char, str) {
+        return str.split(' ').join(char)
+    }
