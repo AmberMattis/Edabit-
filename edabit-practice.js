9c87861170334590b6160844a4be678e5bffd5ac
Amber Mattis <amber.mattis2@gmail.com>
2020-10-25 10:07:42 -0500

    edabit practice

--- edabit-practice.js
+++ edabit-practice.js
@@ -324,3 +324,8 @@
     function stackBoxes(n) {
         return n * n;
     }
+
+// Given a Rubik's Cube with a side length of n, return the number of individual stickers that are needed to cover the whole cube.
+    function howManyStickers(n) {
+        return Math.pow(n,2) * 6;
+    }
