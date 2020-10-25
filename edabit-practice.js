be5da77bb7e0b6ca8d90d7b169fffaffea8df1c3
Amber Mattis <amber.mattis2@gmail.com>
2020-10-25 09:37:00 -0500

    edabit practice

--- edabit-practice.js
+++ edabit-practice.js
@@ -316,3 +316,6 @@
             return base * height
         }
     }
+
+//Create a function that takes a name and returns a greeting in the form of a string. Don't use a normal function, use an arrow function.
+    const helloName = name => "Hello " + name + "!";
