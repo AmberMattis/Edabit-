4a542c5e7c75de893e74645af7eba5c097d39391
Amber Mattis <amber.mattis2@gmail.com>
2020-10-30 19:55:20 -0500

    completed is last character n function

--- edabit-practice.js
+++ edabit-practice.js
@@ -344,3 +344,8 @@
     function concat(arr1, arr2) {
         return arr1.concat(arr2);
     }
+
+// Create a function that takes a string (a random name). If the last character of the name is an "n", return true, otherwise return false.
+    function isLastCharacterN(word) {
+        return word.endsWith("n");
+    }
