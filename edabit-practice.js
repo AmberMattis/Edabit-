2a9ce70f2b45ec992c8fc820bbc24701b5821a11
Amber Mattis <amber.mattis2@gmail.com>
2020-10-30 19:58:52 -0500

    completed find index of item function

--- edabit-practice.js
+++ edabit-practice.js
@@ -349,3 +349,8 @@
     function isLastCharacterN(word) {
         return word.endsWith("n");
     }
+
+// Create a function that searches for the index of a given item in an array. If the item is present, it should return the index, otherwise, it should return -1.
+    function search(arr, item) {
+        return arr.indexOf(item);
+    }
