f9bc5ea7d6d2d5f9fe7781112406aff1fb4cf460
Amber Mattis <amber.mattis2@gmail.com>
2020-11-01 19:58:06 -0600

    edabit practice

--- edabit-practice.js
+++ edabit-practice.js
@@ -360,3 +360,28 @@
         return arguments.length;
     }
 
+//  In the Code tab you will find code that is missing a single character in order to pass the tests. However, your goal is to submit a function as minimalist as possible. Use the tips in the tips section below.
+//
+// Write a function that returns the strings:
+//
+// "both" if both given booleans a and b are true.
+// "first" if only a is true.
+// "second" if only b is true .
+// "neither" if both a and b are false.
+
+    function areTrue(a, b) {
+        if (a == true) {
+            if (b == true) {
+                return "both";
+            }
+            else {
+                return "first";
+            }
+        }
+        else if (b == true) {
+            return "second";
+        }
+        else {
+            return "neither";
+        }
+    }
