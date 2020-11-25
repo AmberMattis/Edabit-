500e98e09aec787c37ca4bb76ff9c4bcd0bc87d2
Amber Mattis <amber.mattis2@gmail.com>
2020-11-24 22:42:35 -0600

    edabit practice

--- edabit-practice.js
+++ edabit-practice.js
@@ -430,3 +430,13 @@
         }
         return sum;
     }
+
+
+//Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+    function sumArray(arr) {
+        var sum = 0;
+        for (var i=0; i<arr.length; i++) {
+            sum+=arr[i]
+        }
+        return sum
+    }
